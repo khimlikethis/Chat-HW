20dd5954f993db7da8ee3b1765e6261fe0d736f7
Auekan <51107129+khimlikethis@users.noreply.github.com>
2019-05-29 01:23:22 +0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -229,7 +229,7 @@ $(function() {
   socket.on('login', (data) => {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
+    var message = "Welcome to Socket.IO Chat – Homework by Auekan";
     log(message, {
       prepend: true
     });
